2ecf431a665b8d08ad51797aa72b9fb71079f3b4
Aaryan658 <65020836+Aaryan658@users.noreply.github.com>
2026-07-04 18:24:47 +0530

    Optimize Florence-2 generation parameters for CPU speed

--- app.py
+++ app.py
@@ -234,9 +234,9 @@ def transcribe(image):
         generated_ids = model.generate(
             input_ids=inputs["input_ids"],
             pixel_values=inputs["pixel_values"],
-            max_new_tokens=1024,
+            max_new_tokens=256,
             do_sample=False,
-            num_beams=3,
+            num_beams=1,
         )
 
     generated_text = processor.batch_decode(generated_ids, skip_special_tokens=False)[0]
